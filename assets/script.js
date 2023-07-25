52c36c6716e2ce0fc9f1d8435efe08702024794e
Villzies <gemberling.stephan@att.net>
2023-07-24 20:08:25 -0500

    relocated files

---
+++ assets/script.js
@@ -0,0 +1,124 @@
+var category = "happiness";
+
+fetch("https://api.api-ninjas.com/v1/quotes?category=" + category, {
+  method: "GET", //GET is the default.
+  headers: { "X-Api-Key": "s6+WMAEYzVKHWRIkfCszQQ==uBXREA4OMYGwbEC8" },
+  contentType: "application/json",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data[0]);
+    var author = data[0].author.toString();
+    console.log(author);
+    var names = author;
+    fetch("https://api.api-ninjas.com/v1/celebrity?name=" + names, {
+      method: "GET",
+      headers: { "X-Api-Key": "s6+WMAEYzVKHWRIkfCszQQ==uBXREA4OMYGwbEC8" },
+      contentType: "application/json",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data[0]);
+        if (data[0] === undefined) {
+          fetch(
+            "https://api.api-ninjas.com/v1/historicalfigures?name=" + names,
+            {
+              method: "GET",
+              headers: {
+                "X-Api-Key": "s6+WMAEYzVKHWRIkfCszQQ==uBXREA4OMYGwbEC8",
+              },
+              contentType: "application/json",
+            }
+          )
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data[0]);
+
+              if (data[0] === undefined) {
+                location.reload();
+              }
+            });
+        }
+      });
+  });
+
+
+function random_category() {
+  let categories = [
+    "age",
+    "alone",
+    "amazing",
+    "anger",
+    "architecture",
+    "art",
+    "attitude",
+    "beauty",
+    "best",
+    "birthday",
+    "business",
+    "car",
+    "change",
+    "communications",
+    "computers",
+    "cool",
+    "courage",
+    "dad",
+    "dating",
+    "death",
+    "design",
+    "dreams",
+    "education",
+    "environmental",
+    "equality",
+    "experience",
+    "failure",
+    "faith",
+    "family",
+    "famous",
+    "fear",
+    "fitness",
+    "food",
+    "forgiveness",
+    "freedom",
+    "friendship",
+    "funny",
+    "future",
+    "god",
+    "good",
+    "government",
+    "graduation",
+    "great",
+    "happiness",
+    "health",
+    "history",
+    "home",
+    "hope",
+    "humor",
+    "imagination",
+    "inspirational",
+    "intelligence",
+    "jealousy",
+    "knowledge",
+    "leadership",
+    "learning",
+    "legal",
+    "life",
+    "love",
+    "marriage",
+    "medical",
+    "men",
+    "mom",
+    "money",
+    "morning",
+    "movies",
+    "success",
+  ];
+  return categories[Math.floor(Math.random() * categories.length)];
+}
+
