89071ffd5f244229dd4f1659f5f594b76b7d76ea
Richard W <richard.warden@protonmail.com>
2023-07-27 20:44:21 -0500

    added wiki previews to saved quotes page

--- assets/saved.js
+++ assets/saved.js
@@ -3,26 +3,41 @@ var quotesEl = document.getElementById("quotes");
 function printQuotes() {
   var savedQuotes =
     JSON.parse(window.localStorage.getItem("SavedQuotes")) || [];
-  savedQuotes.forEach(function (quote) {
+    savedQuotes.forEach(function (quote) {
     var oneQuoteEl = document.createElement("div");
     var quoteline = document.createElement("p");
-    var author = document.createElement("p");
+    var author = document.createElement("a");
     var fact1 = document.createElement("P");
     var fact2 = document.createElement("p");
     quoteline.textContent = quote.quotesX;
-    author.textContent = quote.author;
-    fact2.textContent = "Born: " + quote.dob;
-    fact1.textContent = "Title: " + quote.title.replaceAll("_", " ");
+    author.textContent = "Author: " + quote.name;
+    var wikiAuthor = quote.name.replaceAll(" ", "_");
+    author.href = "https://en.wikipedia.org/wiki/" + wikiAuthor;
     oneQuoteEl.setAttribute(
       "class",
       "card has-background-light mb-3 is-relative is-responsive has-text-centered"
     );
-    oneQuoteEl.appendChild(quoteline);
-    oneQuoteEl.appendChild(author);
-    oneQuoteEl.appendChild(fact1);
-    oneQuoteEl.appendChild(fact2);
-
+    if (quote.dob!="See Wikipedia Article" || quote.title!="See Wikipedia Article"){
+      fact2.textContent = "Born: " + quote.dob;
+      fact1.textContent = "Title: " + quote.title.replaceAll("_", " ");
+      oneQuoteEl.appendChild(quoteline);
+      oneQuoteEl.appendChild(author);
+      oneQuoteEl.appendChild(fact1);
+      oneQuoteEl.appendChild(fact2);
+    }
+    else {
+      oneQuoteEl.appendChild(quoteline);
+      oneQuoteEl.appendChild(author);
+      var seeWiki = document.createElement("p");
+      seeWiki.textContent="See Wikipedia Article for more information";
+      oneQuoteEl.appendChild(seeWiki);
+    }
+    
     quotesEl.appendChild(oneQuoteEl);
+    
+    wikipediaPreview.init({
+      detectLinks: true,
+    });
   });
 }
 
