aaffe78c474e5781a27f639da73855fd82aad5ef
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-07-30 20:07:50 -0500

    fixed background image url

--- assets/saved.js
+++ assets/saved.js
@@ -3,13 +3,13 @@ var quotesEl = document.getElementById("quotes");
 function printQuotes() {
   var savedQuotes =
     JSON.parse(window.localStorage.getItem("SavedQuotes")) || [];
-    savedQuotes.forEach(function (quote) {
+  savedQuotes.forEach(function (quote) {
     var oneQuoteEl = document.createElement("div");
     var quoteline = document.createElement("p");
     var author = document.createElement("a");
     var fact1 = document.createElement("P");
     var fact2 = document.createElement("p");
-    quoteline.textContent = quote.quotesX;
+    quoteline.textContent = '"' + quote.quotesX + '"';
     author.textContent = "Author: " + quote.name;
     var wikiAuthor = quote.name.replaceAll(" ", "_");
     author.href = "https://en.wikipedia.org/wiki/" + wikiAuthor;
@@ -17,24 +17,27 @@ function printQuotes() {
       "class",
       "card m-3 column is-mobile is-offset-one-quarter is-3 is-relative is-responsive has-text-left"
     );
-    if (quote.dob!="See Wikipedia Article" || quote.title!="See Wikipedia Article"){
+    quoteline.setAttribute("class", "subtitle");
+    if (
+      quote.dob != "See Wikipedia Article" ||
+      quote.title != "See Wikipedia Article"
+    ) {
       fact2.textContent = "Born: " + quote.dob;
       fact1.textContent = "Title: " + quote.title.replaceAll("_", " ");
       oneQuoteEl.appendChild(quoteline);
       oneQuoteEl.appendChild(author);
       oneQuoteEl.appendChild(fact1);
       oneQuoteEl.appendChild(fact2);
-    }
-    else {
+    } else {
       oneQuoteEl.appendChild(quoteline);
       oneQuoteEl.appendChild(author);
       var seeWiki = document.createElement("p");
-      seeWiki.textContent="See Wikipedia Article for more information";
+      seeWiki.textContent = "See Wikipedia Article for more information";
       oneQuoteEl.appendChild(seeWiki);
     }
-    
+
     quotesEl.appendChild(oneQuoteEl);
-    
+
     wikipediaPreview.init({
       detectLinks: true,
     });
